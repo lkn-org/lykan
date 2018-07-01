bdec24b963ef67894080bc7342c4b18d14fbabd3
Thomas Letan <contact@thomasletan.fr>
2018-07-01 16:53:32 +0200

    fix: Add missing files

---
+++ example/assets/tilesets/map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="map" tilewidth="16" tileheight="16" tilecount="480" columns="30">
+ <image source="map.png" width="480" height="256"/>
+</tileset>
